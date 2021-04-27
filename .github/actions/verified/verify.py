48fa4f8b2b741cf41e535e02ab9a5f85235df6f3
rvalvers <rvalverde.sanchez@atsistemas.com>
2021-04-27 19:33:19 +0200

    adding workflow

---
+++ .github/actions/verified/verify.py
@@ -0,0 +1,9 @@
+import requests, os
+from bs4 import BeautifulSoup
+
+req = requests.get(os.getenv('INPUT_URLACTION'))
+soup = BeautifulSoup(req.text)
+verify = soup.find_all("svg", {"class": "octicon octicon-verified text-blue-light"})
+
+if not len(verify):
+    print("PELIGRO")
