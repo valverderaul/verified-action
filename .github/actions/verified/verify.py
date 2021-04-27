883d920ab5569f8f6bdaa162da1e4b79299b7d04
rvalvers <rvalverde.sanchez@atsistemas.com>
2021-04-27 19:40:58 +0200

    added parser type

--- .github/actions/verified/verify.py
+++ .github/actions/verified/verify.py
@@ -2,7 +2,7 @@ import requests, os
 from bs4 import BeautifulSoup
 
 req = requests.get(os.getenv('INPUT_URLACTION'))
-soup = BeautifulSoup(req.text)
+soup = BeautifulSoup(req.text, features="html.parser")
 verify = soup.find_all("svg", {"class": "octicon octicon-verified text-blue-light"})
 
 if not len(verify):
