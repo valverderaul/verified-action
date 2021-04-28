3676cf6119ce1c08bed16f81fae8abc83693853d
rvalvers <rvalverde.sanchez@atsistemas.com>
2021-04-28 11:47:53 +0200

    testing

--- .github/actions/verified/verify.py
+++ .github/actions/verified/verify.py
@@ -1,7 +1,7 @@
 import requests, os
 from bs4 import BeautifulSoup
 
-print(os.system('cd /github/workspace/0'))
+print(os.system('cd .github'))
 print("========================")
 print(os.system('pwd'))
 print("========================")
