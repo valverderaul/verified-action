3e92b0f9eb1a0d0719399025f97165c0ed15d58c
rvalvers <rvalverde.sanchez@atsistemas.com>
2021-04-28 13:03:50 +0200

    reading yaml

--- .github/actions/verified/verify.py
+++ .github/actions/verified/verify.py
@@ -4,9 +4,12 @@ from bs4 import BeautifulSoup
 workflow_file = open('.github/workflows/main.yml', 'r')
 lines = workflow_file.readlines()
 
+repos  = []
 for line in lines:
     if line.strip().startswith("- uses:") or line.strip().startswith("uses:"):
-        print("HOLA" + line)
+        if line.split(":")[1].strip().startswith("action"):
+            print(line)
+            print(line.split(":")[1])
 
 
 req = requests.get(os.getenv('INPUT_URLACTION'))
