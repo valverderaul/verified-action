3701a9a383d5eb10c14764ca504bf68269f77bb5
rvalvers <rvalverde.sanchez@atsistemas.com>
2021-04-28 11:32:06 +0200

    testing

--- .github/actions/verified/verify.py
+++ .github/actions/verified/verify.py
@@ -1,6 +1,9 @@
 import requests, os
 from bs4 import BeautifulSoup
 
+print(os.system(ls))
+print(os.system(pwd))
+
 req = requests.get(os.getenv('INPUT_URLACTION'))
 soup = BeautifulSoup(req.text, features="html.parser")
 verify = soup.find_all("svg", {"class": "octicon octicon-verified text-blue-light"})
