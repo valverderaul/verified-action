dbee9bde61c250421e27e3be53cc217be1077c6d
rvalvers <rvalverde.sanchez@atsistemas.com>
2021-04-28 14:11:32 +0200

    finished?

--- .github/actions/verified/verify.py
+++ .github/actions/verified/verify.py
@@ -10,10 +10,11 @@ for line in lines:
         if not line.split(":")[1].strip().startswith("."):
             req_to_repo = requests.get("https://github.com/" + line.split(":")[1].split("@")[0].strip())
             print(req_to_repo)
-            soup = BeautifulSoup(req.text, features="html.parser")
+            soup = BeautifulSoup(req_to_repo.text, features="html.parser")
             action = soup.find("a", string="View on Marketplace")['href']
             print(action)
             req_to_action = requests.get("https://github.com" + action)
+            soup = BeautifulSoup(req_to_action.text, features="html.parser")
             verify = soup.find_all("svg", {"class": "octicon octicon-verified text-blue-light"})
             
             if not len(verify):
