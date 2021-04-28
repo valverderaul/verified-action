1e8d812ee2416433f28e16121d3969654aa88709
rvalvers <rvalverde.sanchez@atsistemas.com>
2021-04-28 11:40:47 +0200

    testing

--- .github/actions/verified/verify.py
+++ .github/actions/verified/verify.py
@@ -2,7 +2,10 @@ import requests, os
 from bs4 import BeautifulSoup
 
 print(os.system('cd /github/workspace'))
+print("========================")
 print(os.system('pwd'))
+print("========================")
+print(os.system('ls'))
 
 req = requests.get(os.getenv('INPUT_URLACTION'))
 soup = BeautifulSoup(req.text, features="html.parser")
